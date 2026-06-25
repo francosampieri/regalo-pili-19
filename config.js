fb39dd62455e30fb9de757c67e943edf3d1ccb2c
francosampieri <francosampiee@gmail.com>
2026-06-25 17:17:40 -0300

    apiKey

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 //  Pegá tu API key acá y no lo toques más.
 // ══════════════════════════════════════════════════════════
 
-const GEMINI_API_KEY = "TU_API_KEY_ACÁ"; // ← pegá tu key acá y guardá
+const GEMINI_API_KEY = "AQ.Ab8RN6K6mKmiAFs-RyEUrokcaEKBYA1A6ZOU0rRmomufaXXo-g"; // ← pegá tu key acá y guardá
